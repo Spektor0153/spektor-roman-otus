2b3578c878b6349fec88a9994792f8c787ca512c
Spektor0153 <fas-@mail.ru>
2019-07-06 02:51:48 +0400

    Update javascript-1.js

--- javascript-1.js
+++ javascript-1.js
@@ -1,6 +1,11 @@
+
 var sum = (number, iter = 0) => {
     if (!number) return iter;
     return x => sum(x, iter + number);
 }
 
 sum(5)(10)(15)(); 
+
+/* 2 вариант */
+var sum = iter => number => number? sum(iter + number) : iter;
+sum(5)(10)(15)();
